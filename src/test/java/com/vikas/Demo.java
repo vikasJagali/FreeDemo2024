134865a4ee7255ad50652d1d99b80c4181f69b3b
vikas <vikasjagali0@gmail.com>
2024-12-28 12:39:39 +0530

    demo

---
+++ src/test/java/com/vikas/Demo.java
@@ -0,0 +1,53 @@
+package com.vikas;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	WebDriver driver;
+
+	@Test
+	public void doWalk() {
+
+		driver.get("https://www.google.co.in/");
+		WebElement ele = driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
+		ele.sendKeys("vikas");
+
+		ele.sendKeys(Keys.ENTER);
+
+	}
+
+	@Test
+	public void enterText() {
+
+		driver.get("https://www.google.co.in/");
+		WebElement ele = driver.findElement(By.xpath("//textarea[@id='APjFqb']"));
+		ele.sendKeys("jAGALI");
+
+		ele.sendKeys(Keys.ENTER);
+	}
+
+	@BeforeMethod
+	public void init() {
+
+		driver = new ChromeDriver();
+
+	}
+
+	@AfterMethod
+	public void teardown() throws InterruptedException {
+
+		Thread.sleep(Duration.ofSeconds(4));
+		driver.quit();
+	}
+
+}
